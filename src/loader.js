6786be63c2cd0fc486561f7033cf7e15ce78e92c
mccman1519 <vt.banzaj@gmail.com>
2024-05-13 00:08:07 +0300

    Next check attempt[3]

--- src/loader.js
+++ src/loader.js
@@ -162,7 +162,7 @@ const loadResources = (selector, { rawHtmlData }, url, targetDir, timeout = 3000
                 fs
                   .writeFile(
                     absFilename,
-                    typeof response.data === 'string' ? `${response.data}\n\n` : response.data,
+                    typeof response.data === 'string' ? response.data.toString() : response.data,
                     encoding,
                   )
                   .catch((err) => {
