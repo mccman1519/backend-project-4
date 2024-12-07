86d5ccc80d36bde3990c08b3f3588a11e9e1d20d
mccman1519 <vt.banzaj@gmail.com>
2024-12-08 00:13:38 +0300

    Fix last line: set encoding to binary everywhere

--- src/loader.js
+++ src/loader.js
@@ -114,12 +114,12 @@ const loadResources = (selector, { rawHtmlData }, url, targetDir, timeout = 3000
     script: {
       srcType: 'src',
       responseType: 'text',
-      encoding: 'utf-8',
+      encoding: /* 'utf-8' */'binary',
     },
     link: {
       srcType: 'href',
       responseType: 'text',
-      encoding: 'utf-8',
+      encoding: /* 'utf-8' */'binary',
     },
   };
 
@@ -162,7 +162,7 @@ const loadResources = (selector, { rawHtmlData }, url, targetDir, timeout = 3000
                 fs
                   .writeFile(
                     absFilename,
-                    typeof response.data === 'string' ? `${response.data.toString()}`/* .replace(/\n$/, '') */ : response.data,
+                    /* typeof response.data === 'string' ? response.data.toString().replace(/\n$/, '') : */response.data,
                     encoding,
                   )
                   .catch((err) => {
